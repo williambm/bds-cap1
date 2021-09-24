222f9bb236fa2ee6d607cff0f683f6314b1fd889
William B. Montini <williambm@gmail.com>
2021-09-24 08:34:53 -0300

    First Commit of Project

---
+++ src/main/java/com/devsuperior/bdscap1/BdsCap1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.bdscap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsCap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdsCap1Application.class, args);
+	}
+
+}
